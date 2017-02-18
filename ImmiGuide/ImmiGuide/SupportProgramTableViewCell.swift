55abc3e0816a389a731372ab4a7aac755646253c
Shashi <liyanagemadushani21@gmail.com>
2017-02-18 16:31:25 -0500

    Assets

--- ImmiGuide/ImmiGuide/SupportProgramTableViewCell.swift
+++ ImmiGuide/ImmiGuide/SupportProgramTableViewCell.swift
@@ -12,7 +12,7 @@ class SupportProgramTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
