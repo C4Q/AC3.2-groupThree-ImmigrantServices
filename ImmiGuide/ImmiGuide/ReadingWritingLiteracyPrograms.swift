118af87fe8a1333c554998ac11870e223f6b4e84
Cristopher Chavez <cristopherchavezny@gmail.com>
2017-02-18 11:41:13 -0500

    Models for geolocation and location added

---
+++ ImmiGuide/ImmiGuide/ReadingWritingLiteracyPrograms.swift
@@ -0,0 +1,18 @@
+//
+//  ReadingWritingLiteracyPrograms.swift
+//  ImmiGuide
+//
+//  Created by Cris on 2/18/17.
+//  Copyright © 2017 Madushani Lekam Wasam Liyanage. All rights reserved.
+//
+
+import Foundation
+
+class ReadingWritingLiteracyPrograms {
+    var agencyName: String
+    var agencyLocation: Location
+    var agencyPhoneNumber: String
+    var ageGroup: String
+    
+  
+}
